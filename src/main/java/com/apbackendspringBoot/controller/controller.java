472386d362f6fcc6828952cd37e62ab207809724
Carolina perez <mariacarola46@hotmail.com>
2022-07-24 09:50:00 -0300

    Agregue CrossOrigin en el controlador

--- src/main/java/com/apbackendspringBoot/controller/controller.java
+++ src/main/java/com/apbackendspringBoot/controller/controller.java
@@ -4,6 +4,7 @@ import com.apbackendspringBoot.model.Educacion;
 import com.apbackendspringBoot.service.IEducacionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class controller {
     
     @Autowired
